39ea5b75e051bd148a5396677990d2bcb2378992
Vladimir Turnaev <turnaev@gmail.com>
2016-11-16 19:57:00 +0700

    ~

--- src/Command/DevCommand.php
+++ src/Command/DevCommand.php
@@ -18,7 +18,39 @@ class DevCommand extends BaseCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
 
-        v(1);
+        $treeBuilder = new \Symfony\Component\Config\Definition\Builder\TreeBuilder();
+        $rootNode = $treeBuilder->root('database');
+
+        $rootNode
+            ->children()
+                ->booleanNode('auto_connect')
+                    ->defaultTrue()
+                ->end()
+                ->scalarNode('default_connection')
+                    ->defaultValue('default')
+                ->end()
+                ->scalarNode('test')
+                    ->defaultValue('default')
+                ->end()
+            ->end();
+
+
+        $config1 = \Symfony\Component\Yaml\Yaml::parse(
+            file_get_contents(__DIR__.'/test.yml')
+        );
+
+        v($config1);
+        $configs = array($config1);
+
+        $processor = new \Symfony\Component\Config\Definition\Processor();
+
+        $r = $processedConfiguration = $processor->process(
+            $treeBuilder->buildTree(),
+            $configs
+        );
+
+        v($r);
+
 
     }
 }
